ea9dae2b0ac3cfe74903a12949aca4fe934cfff3
zylinktech <root@zylinktech.net>
2024-09-06 00:27:32 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -7,7 +7,5 @@ sudo apt upgrade -y bridge-utils
 sudo apt upgrade -y virt-manager
 sudo apt upgrade -y cpu-checker
 sudo apt upgrade -y whiptail
-cd ~
 sudo rm -rf ./kvm-utility && sudo git clone https://github.com/zylinktech/kvm-utility/new/main
-cd ./kvm-utility
 sudo kvm-ok
