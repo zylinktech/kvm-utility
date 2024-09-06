2c3518873c4b91116101cc7f7d2115505944b8dd
zylinktech <root@zylinktech.net>
2024-09-05 22:37:30 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt update
+sudo apt upgrade -y
+
+sudo apt install -y qemu-kvm
+sudo apt install -y libvirt-daemon-system
+sudo apt install -y libvirt-clients
+sudo apt install -y bridge-utils
+sudo apt install -y virt-manager
+sudo apt install -y cpu-checker
+
+sudo apt install -y whiptail
+
+sudo kvm-ok
